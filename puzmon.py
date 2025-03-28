bdc648c23b60bc3e9941cb461dadf21397d0a1b8
futa <futa92679@gmail.com>
2025-03-28 14:31:37 +0900

    sixth-commit

--- puzmon.py
+++ puzmon.py
@@ -1,87 +1,209 @@
+# インポート
 import random
 
-ELEMENT_SYMBOLS = {
-    '火': '$', '水': '~', '風': '@', '土': '#', '命': '&', '無': ' '
+# グローバル変数の宣言
+ELEMENT_SYMBOLS={
+        '火':'$',
+        '水':'~',
+        '風':'@',
+        '土':'#',
+        '命':'&',
+        '無':' ',
 }
-ELEMENT_COLORS = {
-    '火': 1, '水': 6, '風': 2, '土': 3, '命': 5, '無': 7
+ELEMENT_COLORS={
+        '火':1,
+        '水':6,
+        '風':2,
+        '土':3,
+        '命':5,
+        '無':7,
 }
+
+# 関数宣言
 
 def main():
-    friends = [
-        {'name': '青龍', 'hp': 150, 'max_hp': 150, 'element': '風', 'ap': 15, 'dp': 10},
-        {'name': '朱雀', 'hp': 150, 'max_hp': 150, 'element': '火', 'ap': 25, 'dp': 10},
-        {'name': '白虎', 'hp': 150, 'max_hp': 150, 'element': '土', 'ap': 20, 'dp': 5},
-        {'name': '玄武', 'hp': 150, 'max_hp': 150, 'element': '水', 'ap': 20, 'dp': 15}
+    friends=[
+            {
+                'name':'青龍',
+                'hp':150,
+                'max_hp':150,
+                'element':'風',
+                'ap':15,
+                'dp':10
+            },
+            {
+                'name':'朱雀',
+                'hp':150,
+                'max_hp':150,
+                'element':'火',
+                'ap':25,
+                'dp':10
+            },
+            {
+                'name':'白虎',
+                'hp':150,
+                'max_hp':150,
+                'element':'土',
+                'ap':20,
+                'dp':5
+            },
+            {
+                'name':'玄武',
+                'hp':150,
+                'max_hp':150,
+                'element':'水',
+                'ap':20,
+                'dp':15
+            },
+
     ]
-    monster_list = [
-        {'name': 'スライム', 'hp': 100, 'max_hp': 100, 'element': '水', 'ap': 10, 'dp': 1},
-        {'name': 'ゴブリン', 'hp': 200, 'max_hp': 200, 'element': '土', 'ap': 20, 'dp': 5}
+    monster_list=[
+            {
+                'name':'スライム',
+                'hp':100,
+                'max_hp':100,
+                'element':'水',
+                'ap':10,
+                'dp':1
+            },
+            {
+                'name':'ゴブリン',
+                'hp':200,
+                'max_hp':200,
+                'element':'土',
+                'ap':20,
+                'dp':5
+            },
+            {
+                'name':'オオコウモリ',
+                'hp':300,
+                'max_hp':300,
+                'element':'風',
+                'ap':30,
+                'dp':10
+            },
+            {
+                'name':'ウェアウルフ',
+                'hp':400,
+                'max_hp':400,
+                'element':'風',
+                'ap':40,
+                'dp':15
+            },
+            {
+                'name':'ドラゴン',
+                'hp':600,
+                'max_hp':600,
+                'element':'火',
+                'ap':50,
+                'dp':20
+            },
     ]
-    player_name = input('プレイヤー名を入力してください>> ')
+    while True:
+        player_name=input('プレイヤー名を入力してください>>')
+        if len(player_name) > 0:
+            break
+        print('エラー:プレイヤー名を入力してください')
+
     print('*** Puzzle & Monsters ***')
-    party = organize_party(player_name, friends)
-    kills = go_dungeon(party, monster_list)
+    party=organize_party(player_name,friends)
+    kills = go_dungeon(party,monster_list)
+    if kills == len(monster_list):
+        print('*** GAME CLEARED!! ***')
+    else:
+        print('*** GAME OVER!! ***')
+
     print(f'倒したモンスター数={kills}')
 
-def organize_party(player_name, friends):
-    total_hp = sum(friend['hp'] for friend in friends)
-    total_dp = sum(friend['dp'] for friend in friends) / len(friends)
-    return {'name': player_name, 'friends': friends, 'hp': total_hp, 'max_hp': total_hp, 'dp': total_dp}
 
-def go_dungeon(party, monster_list):
+def go_dungeon(party,monster_list):
     kills = 0
-    print(f'{party["name"]}のパーティ(HP={party["hp"]})はダンジョンに到着した')
+    print(f'{party['name']}のパーティ(HP={party['hp']})はダンジョンに到着した')
     show_party(party)
     for monster in monster_list:
-        show_battlefield(party, monster)
-        kills += do_battle(party, monster)
+        kills += do_battle(party,monster)
         if party['hp'] <= 0:
-            print(f'{party["name"]}はダンジョンから逃げ出した')
+            print(f'{party['name']}はダンジョンから逃げ出した')
             break
-        print(f'{party["name"]}はさらに奥に進んだ')
+        print(f'{party['name']}はさらに奥に進んだ')
+        print('==================================')
+    else:
+        print(f'{party['name']}はダンジョンに制覇した')
+
     return kills
 
-def do_battle(party, monster):
-    print(f'【{party["name"]}のターン(HP={party["hp"]})】')
-    show_battlefield(party, monster)
+def do_battle(party,monster):
+    print_monster_name(monster)
+    print('が現れた!')
     
-    command = input('コマンド?>> ')
-    if command:
-        damage = sum(friend['ap'] for friend in party['friends']) - monster['dp']
-        monster['hp'] = max(monster['hp'] - damage, 0)
-        print(f'{monster["name"]}に{damage}のダメージを与えた')
-    
-    if monster['hp'] > 0:
-        print(f'【{monster["name"]}のターン(HP={monster["hp"]})】')
-        enemy_damage = max(monster['ap'] - party['dp'], 1)
-        party['hp'] = max(party['hp'] - enemy_damage, 0)
-        print(f'敵の攻撃！ {enemy_damage}のダメージを受けた')
+    while True:
+        on_player_turn(party,monster)
+        if monster['hp'] <= 0:
+            break
+        on_enemy_turn(party,monster)
         if party['hp'] <= 0:
-            print(f'{party["name"]}は倒れた...')
+            print('パーティのHPは0になった')
             return 0
-    else:
-        print(f'{monster["name"]}を倒した！')
-        return 1
+        
+    print_monster_name(monster)
+    print('を倒した!')
+    return 1
 
-def show_battlefield(party, monster):
-    print(f'バトルフィールド')
-    print(f'{monster["name"]} HP = {monster["hp"]} / {monster["max_hp"]}')
-    show_party(party)
-    print('----------------------------')
-    show_puzzle_board()
+def print_monster_name(monster):
+    monster_name=monster['name']
+    symbol = ELEMENT_SYMBOLS[monster['element']]
+    color=ELEMENT_COLORS[monster['element']]
+
+    #モンスター名を表示
+    print(f'\033[3{color}m{symbol}{monster_name}{symbol}\033[0m',end='')
+
+def organize_party(player_name,friends):
+    total_hp = 0
+    total_dp = 0
+    for friend in friends:
+        total_hp += friend['hp']
+        total_dp += friend['dp']
+
+    party = {
+            'name':player_name,
+            'friends':friends,
+            'hp':total_hp,
+            'max_hp':total_hp,
+            'dp':total_dp / len(friends)
+    }
+    return party
 
 def show_party(party):
-    print(f'【{party["name"]}のパーティ】 HP = {party["hp"]} / {party["max_hp"]}')
+    print('<パーティ編成>----------------------')
     for friend in party['friends']:
-        print(f'{friend["name"]} (HP={friend["hp"]}, 攻撃={friend["ap"]}, 防御={friend["dp"]})')
+        print_monster_name(friend)
+        print(f' HP= {friend['hp']} 攻撃={friend['ap']} 防御= {friend['dp']}')
+    print('------------------------------------')
+    print()
 
-def show_puzzle_board():
-    elements = ['火', '水', '風', '土']
-    board = [[random.choice(elements) for _ in range(6)] for _ in range(5)]
-    print('  A B C D E F')
-    for row in board:
-        print(' '.join(ELEMENT_SYMBOLS[e] for e in row))
 
-if __name__ == '__main__':
-    main()
+def on_player_turn(party,monster):
+    print(f'\n【{party['name']}のターン】(HP={party['hp']})')
+    command = input('コマンド? >')
+    do_attack(monster,command)
+
+def on_enemy_turn(party,monster):
+    print(f'\n【',end='')
+    print_monster_name(monster)
+    print(f'のターン】(HP={monster['hp']})')
+    do_enemy_attack(party)
+def do_attack(monster,command):
+    damage = int(hash(command)) % 50
+    rand = random.uniform(-0.1,0.1)+1
+    damage = int(damage * rand)
+    print(f'{damage}のダメージを与えた')
+    monster['hp'] -= damage
+def do_enemy_attack(party):
+    damage = 200
+    print(f'{damage}のダメージを受けた')
+    party['hp'] -= damage
+
+
+# main関数の呼び出し
+main()
+
